8085c0d4a899bebdbece59f8253106a89959aee9
karthikeyan <karthirm90@gmail.com>
2021-11-09 14:06:18 +0800

    updated mysql properties

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,4 @@
-insert into user values(1, 'test', 'test', 'admin');
+/*insert into user values(1, 'test', 'test', 'admin');
 insert into user values(2, 'user1', 'user1', 'user');
-
-insert into book(id,name,author,price) values (1,'Century','Ganguly',24);
+insert into book(id,name,author,price) values (1,'Century','Ganguly',24); */
 
